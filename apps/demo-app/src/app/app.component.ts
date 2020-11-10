41c79c811863bb5592b6a973d1a5f78373e456ab
tehwolf <robert.weyres@conciso.de>
2020-11-10 15:35:44 +0100

    Use BackendService

--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@nx-demo-app/api-interfaces';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BackendService } from './backend.service';
 
 @Component({
   selector: 'nx-demo-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit {
+  hello$: Observable<{ message: string }>;
+  constructor(private backendService: BackendService) {}
+
+  ngOnInit(): void {
+    this.hello$ = this.backendService.getWelcomeMessage();
+  }
 }
