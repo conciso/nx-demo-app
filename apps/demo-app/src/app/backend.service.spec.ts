3b29d21682c5a1a0b781af1e6dce48a2c1c13c9e
tehwolf <robert.weyres@conciso.de>
2020-11-10 16:53:18 +0100

    Add service test

--- apps/demo-app/src/app/backend.service.spec.ts
+++ apps/demo-app/src/app/backend.service.spec.ts
@@ -1,16 +1,27 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
 
 describe('BackendService', () => {
+  let httpMock: HttpTestingController;
   let service: BackendService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(BackendService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get the welcome message', () => {
+    service.getWelcomeMessage().subscribe();
+
+    httpMock.expectOne('http://localhost:4444/api/hello');
   });
 });
