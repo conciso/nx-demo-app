d0cadab65436b2b57b4b12b712f12e2ac6d95034
tehwolf <robert.weyres@conciso.de>
2020-11-10 15:13:52 +0100

    Use port from environment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,20 +2,21 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = environment.api_port;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log(
+      'Listening at localhost:' + environment.api_port + '/' + globalPrefix
+    );
   });
 }
-
 bootstrap();
