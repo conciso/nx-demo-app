4af43346ef9c3212258b5fca2b1771c2ac2f301e
tehwolf <robert.weyres@conciso.de>
2020-11-10 15:11:51 +0100

    Link shared environments

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,3 @@
-export const environment = {
-  production: false,
-};
+import { environment as _environment } from '@nx-demo-app/environments/environment';
+
+export const environment = _environment;
--- apps/demo-app/src/environments/environment.prod.ts
+++ apps/demo-app/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
-export const environment = {
-  production: true,
-};
+import { environment as _environment } from '@nx-demo-app/environments/environment.prod';
+
+export const environment = _environment;
